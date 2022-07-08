69687b61925818f24cab693447b35bf4fd066a75
LobsterFestival <jeremylocatelli1995@gmail.com>
2022-07-07 20:14:38 -0700

    collision checks working and basic gameloop created.

--- source/main.lua
+++ source/main.lua
@@ -17,35 +17,34 @@ local SCREENHEIGHT <const> = playdate.display.getHeight()
 local SCREENWIDTH <const> = playdate.display.getWidth()
 
 local speedModifier = 0
-player = { sprite = nil, speed = 4, modifiedSpeed = 0, timer = nil, health = 3 }
+
+player = {sprite = nil, speed = 4, modifiedSpeed = 0, timer = nil, health = 3}
 
 -- This was in example code, we might still use it.
-local playTimer = nil
-local playTime = 30 * 1000
+-- local playTimer = nil
+-- local playTime = 30 * 1000
+-- local function resetTimer()
+-- 	playTimer = playdate.timer.new(playTime, playTime, 0, playdate.easingFunctions.linear)
+-- end
 
 -- Dont know if we are doing score but its here
 local score = 0
-
 -- 										#### END GLOBALS #####
 
-obstaclesOnScreen = {}
-
-local function resetTimer()
-	playTimer = playdate.timer.new(playTime, playTime, 0, playdate.easingFunctions.linear)
-end
-
 local function initPlayer()
+	player.health = 3
 	math.randomseed(playdate.getSecondsSinceEpoch())
 	-- Loads image of sprite and sets that to Sprite object
 	local playerImage = gfx.image.new("images/player")
 	player.sprite = gfx.sprite.new(playerImage)
+	player.sprite:setGroups(1)
 	-- default location
 	player.sprite:moveTo(200, 120)
 	-- set collider
 	player.sprite:setCollideRect(0, 0, player.sprite:getSize())
 	-- actually put it into the game
 	player.sprite:add()
-	resetTimer()
+
 end
 
 -- Initilize and draw to screen all UI/HUD elements, including screen safe zone so player cant drive over the HUD
@@ -59,11 +58,6 @@ local rightSpawnerTimer = nil
 local bottomSpawnerTimer = nil
 function initGameState()
 	-- background image init
-	-- decorations init
-
-	-- start timer for spawning objects into gamefield
-	-- TODO: need to make the timing more dynamic, this will spawn at a constant rate, LAME
-	-- Callback function used by obstacle timer to handle spawning and adding obstacles to array
 	local function rightObjectTimerCallback()
 		print("R timer callback fired~!")
 		-- object is now added to game offscreen
@@ -78,11 +72,13 @@ function initGameState()
 		print("Spawned bottom object " .. obs.name.. " at " .. obs.x .. "," .. obs.y)
 	end
 
+	-- start timer for spawning objects into gamefield
+	-- TODO: need to make the timing more dynamic, this will spawn at a constant rate, LAME
+	-- Callback function used by obstacle timer to handle spawning and adding obstacles to array
 	local delayTimeInital = math.random(800, 1100) -- 1.5 - 3 seconds
 	local delayTimeSecondary = math.random(800, 1100) -- 1.5 - 4 seconds
 	rightSpawnerTimer = playdate.timer.keyRepeatTimerWithDelay(delayTimeInital, delayTimeSecondary, rightObjectTimerCallback)
 	bottomSpawnerTimer = playdate.timer.keyRepeatTimerWithDelay(delayTimeInital, delayTimeSecondary, bottomObjectTimerCallback)
-
 end
 
 -- TODO: Need to have function that handles background image changing
@@ -92,22 +88,25 @@ initPlayer()
 initHUD()
 initGameState()
 
+gameOverText = ""
 -- This function is called 30 times a second and is where the main game logic takes place
 function playdate.update()
-	-- Time is up!
-	-- End game handling should be done in another function as well
-	if playTimer.value == 0 then
-		-- Press A, restarts game
-		-- TODO: obvs update for out own reset stuff
+	-- will be handling crank stuff up here since the speed values affect everything more or less
+	-- the .0x is just to make the speedramp up less while still being finely tunable
+	-- -2 is minimum modifier 8 is maximum
+
+	-- PLACEHOLDER: finish end state handling, the game over text is a super hack for now
+	if player.health == 0 then
+		gameOverText = "GAME OVER \nPress A"
+		rightSpawnerTimer:remove()
+		bottomSpawnerTimer:remove()
 		if playdate.buttonJustPressed(playdate.kButtonA) then
-			resetTimer()
-			score = 0
+			player.sprite:remove()
+			gameOverText = ""
+			initPlayer()
+			initGameState()
 		end
-		-- Main Game Loop, player movement and coin pick up
 	else
-		--will be handling crank stuff up here since the speed values affect everything more or less
-		-- the .0x is just to make the speedramp up less while still being finely tunable
-		-- -2 is minimum modifier 8 is maximum
 		speedModifier += playdate.getCrankTicks(60) * .04
 		if speedModifier > 8 then
 			speedModifier = 8;
@@ -130,19 +129,14 @@ function playdate.update()
 		if playdate.buttonIsPressed(playdate.kButtonLeft) then
 			player.sprite:moveBy(-player.modifiedSpeed, 0)
 		end
-		-- Crank handling goes here
+
+		gfx.sprite.update()
+
+		-- 3 args, text, x, y
+		-- this'll be used for fine tuning my shit
+		gfx.drawText("speed: " .. player.modifiedSpeed, 5, 30)
+		
 	end
-
-
-	gfx.sprite.update()
 	playdate.timer.updateTimers()
-	-- Look into exactly what this call does
-
-
-	-- 3 args, text, x, y
-	-- We should abstract UI updates to another function as well
-	gfx.drawText("Time: " .. math.ceil(playTimer.value / 1000), 5, 5)
-	gfx.drawText("Score: " .. score, 320, 5)
-	-- this'll be used for fine tuning my shit
-	gfx.drawText("speed: " .. player.modifiedSpeed, 5, 30)
+	gfx.drawText(gameOverText, 180, 120)
 end
--- source/obstacles.lua
+++ source/obstacles.lua
@@ -18,7 +18,8 @@ local RSPAWN_LANE2 <const> = 120
 local RSPAWN_LANE3 <const> = 180
 
 local LANES = {RSPAWN_LANE1,RSPAWN_LANE2,RSPAWN_LANE3}
-
+local PLAYER_SPRITE_GROUP = 1
+local OBSTACLE_SPRITE_GROUP = 2
 -- PLACEHOLDER: Fucking playdate doesn't tell you if it failed to load an image, the function
 -- will simply return nil, and sprite doesn't complain on loading nil objects...
 -- register sprites through this function so if you messed up the path, YOU'LL KNOW!
@@ -31,49 +32,54 @@ function loadImageToSpriteHelper(path)
     return image
 end
 
--- Luas way of defining a class structure
-local function customClass(base)
-    local t = base and setmetatable({}, base) or {}
-    t.__index = t -- look up keys in base class if not found in instance
-    t.__class = t -- Inherited Class
-    t.__base  = base -- Base Class
+-- NOTE: keeping this around in case we need it
+-- one way of defining a class structure
+-- local function customClass(base)
+--     local t = base and setmetatable({}, base) or {}
+--     t.__index = t -- look up keys in base class if not found in instance
+--     t.__class = t -- Inherited Class
+--     t.__base  = base -- Base Class
 
-    function t.new(...)
-      local o = setmetatable({}, t)
-      if o.__init then
-        if t == ... then -- we call as Class:new()
-            o:__init(select(2, ...))
-            return o
-        else             -- we call as Class.new()
-            o:__init(...)
-            return o
-        end
-      end
-      return o
-    end
-    return t
-end
+--     function t.new(...)
+--       local o = setmetatable({}, t)
+--       if o.__init then
+--         if t == ... then -- we call as Class:new()
+--             o:__init(select(2, ...))
+--             return o
+--         else             -- we call as Class.new()
+--             o:__init(...)
+--             return o
+--         end
+--       end
+--       return o
+--     end
+--     return t
+-- end
 
--- Base Object class
-Object = customClass()
+-- -- Base Object class
+-- Object = customClass()
 
--- Tumbleweed class inherited from Object 
-Obstacle = customClass(Object)
+-- -- Subclass inherited from Object 
+-- SubClass = customClass(Object)
+
 
 -- Subclass off of playdates sprite class so we can override update()
 -- then when playdat.graphics.sprite.update (notice the . not :) all sprites
 -- will call :update(), we can then impliment our own logic in here to run every frame
 class('ObsSprite').extends(playdate.graphics.sprite)
 
--- PLACEHOLDER: until collision detection is worked out
-local playerHit = false
-
 function ObsSprite:update()
-    print("I'm updating!!")
-    print("My speed is: "..self.currentSpeed.." My zone "..self.zone)
+    local playerHit = false
+    local collisions = self:overlappingSprites()
+    if #collisions > 0 then
+        playerHit = true
+    end
     if playerHit == true then
         -- deduct health from player
+        print("Player loses 1 health :(")
+        player.health -= 1
         -- despawn object
+        self:remove()
     else
         -- what kind of object are we dealing with
         if self.zone == "right" then
@@ -96,9 +102,16 @@ end
 -- Factory for Obstacle which is a subclass of sprite with 
 -- parameters unique to this subclass
 -- Takes a table from rightSpawningObjects or bottomSpawningObjects table containing obstacle information
+-- Obstacles are part of sprite group 2, player is sprite group 1 
 function createObstacle(obsInfo)
     local obs = ObsSprite()
+    obs:setGroups(OBSTACLE_SPRITE_GROUP)
+    obs:setCollidesWithGroups(PLAYER_SPRITE_GROUP)
     obs:setImage(loadImageToSpriteHelper(obsInfo.image))
+    -- set collision rect to same size as sprite itself (determined by image size)
+    obs:setCollideRect(0,0,obs:getSize())
+    -- collision type that will stop movement on hit (?)
+    obs.collisionResponse = playdate.graphics.sprite.kCollisionTypeFreeze
     obs.name = obsInfo.name
     obs.currentSpeed = 0
     obs.speedModifier = 0
@@ -106,18 +119,11 @@ function createObstacle(obsInfo)
     return obs
 end
 
-
--- PLACEHOLDER: update art assets when complete
-bottomObsImage = loadImageToSpriteHelper("images/bobject_ph")
-bottomObsSprite = gfx.sprite.new(bottomObsImage)
-
-rightObsImage = loadImageToSpriteHelper("images/robject_ph")
-rightObsSprite = gfx.sprite.new(rightObsImage)
-
 -- TODO: sprite initilization for other objects go below (decorations, etc)
+-- decorations can use base gfx.sprite class
 
 --                                           ##### END GLOBALS #####
-
+-- PLACEHOLDER: update art assets when complete
 rightSpawningObjects = {{name="pothole",image="images/bobject_ph",zone="right"}}
 bottomSpawningObjects = {{name="tumbleweed",image="images/robject_ph",zone="bottom"}}
 
